8713e616839cd23495961c6e678e445b3156a744
Juanje Cilla <juanje.cilla@gmail.com>
2024-09-06 10:49:48 +0200

    Store apk

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,4 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.hilt) apply false
-}
+    id("com.google.gms.google-services") version "4.4.2" apply false}
